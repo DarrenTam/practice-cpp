5e616f5212f0bfddfe084b1a5299deb0327cb773
Darren Tam <darrentamhk@gmail.com>
2021-08-08 17:59:24 +0800

    added test case

--- binary-search/binary_search_unittest.cpp
+++ binary-search/binary_search_unittest.cpp
@@ -16,6 +16,11 @@ namespace {
         EXPECT_EQ(binarySearch(nums, 77), 77);
     }
 
+    TEST(binarySeachCase3, BasicAssertions) {
+        std::vector<int> nums = {1, 2, 5, 8, 34, 60, 77};
+        EXPECT_EQ(binarySearch(nums, 8), 8);
+    }
+
 
     TEST(binarySeachCaseFail, BasicAssertions) {
         std::vector<int> nums = {1, 2, 5, 8, 34, 60, 77};
